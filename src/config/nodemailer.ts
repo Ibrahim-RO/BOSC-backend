a902ff8e75ff3d3b0d2826434edf7198b3b77092
ibrahim <ibra.rodriguez.olaya@gmail.com>
2025-10-08 12:38:59 -0600

    Feat: Implementación de envios de a 2 correos

--- src/config/nodemailer.ts
+++ src/config/nodemailer.ts
@@ -2,12 +2,22 @@ import nodemailer from 'nodemailer'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const transport = nodemailer.createTransport({
+export const transport1 = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 465,
     secure: true,
     auth: {
-        user: process.env.GMAIL_USER,        // tu correo Gmail
-        pass: process.env.GMAIL_APP_PASS     // App Password generado en Gmail
+        user: process.env.GMAIL_USER1,        
+        pass: process.env.GMAIL_APP_PASS1     
+    }
+})
+
+export const transport2 = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.GMAIL_USER2,        
+        pass: process.env.GMAIL_APP_PASS2
     }
 })
--- src/emails/Email.ts
+++ src/emails/Email.ts
@@ -1,4 +1,4 @@
-import { transport } from "../config/nodemailer";
+import { transport1, transport2 } from "../config/nodemailer";
 
 type UserType = {
     fullName: string,
@@ -12,9 +12,33 @@ export class Email {
 
     static sendEmail = async (data: UserType) => {
         try {
-            const email = transport.sendMail({
-                from: 'rodriguez.o.ibra@gmail.com',
-                to: 'rodriguez.o.ibra@gmail.com',
+            const email1 = transport1.sendMail({
+                from: process.env.GMAIL_USER1,
+                to: process.env.GMAIL_USER1,
+                subject: 'Nueva solicitud de contacto',
+                html: `
+                    <div style="font-family: Arial, sans-serif; line-height:1.6; color:#333;">
+                        <h2 style="color:#0056b3;">Nueva solicitud de contacto</h2>
+                        <p>Hola equipo de BOSC,</p>
+                        <p>Se recibió una nueva solicitud de un cliente con la siguiente información:</p>
+                        
+                        <h3>👤 Datos del cliente</h3>
+                        <ul>
+                            <li><strong>Nombre:</strong> ${data.fullName}</li>
+                            <li><strong>Correo:</strong> ${data.email}</li>
+                            <li><strong>Estado:</strong> ${data.state}</li>
+                            <li><strong>Servicio:</strong> ${data.service}</li>
+                            <li><strong>Mensaje:</strong> ${data.message}</li>
+                        </ul>
+
+                        <p style="margin-top:20px;">Saludos,<br/>Sistema BOSC</p>
+                    </div>
+                `
+            })
+
+            const email2 = transport2.sendMail({
+                from: process.env.GMAIL_USER2,
+                to: process.env.GMAIL_USER2,
                 subject: 'Nueva solicitud de contacto',
                 html: `
                     <div style="font-family: Arial, sans-serif; line-height:1.6; color:#333;">
